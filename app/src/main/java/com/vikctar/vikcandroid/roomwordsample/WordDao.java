8284154274ec2aec8e07670562fe2eca7b2a5fb7
Vikctar <vikc.steve@gmail.com>
2019-08-13 00:08:15 +0300

    using the LiveData class

--- app/src/main/java/com/vikctar/vikcandroid/roomwordsample/WordDao.java
+++ app/src/main/java/com/vikctar/vikcandroid/roomwordsample/WordDao.java
@@ -1,5 +1,6 @@
 package com.vikctar.vikcandroid.roomwordsample;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
@@ -17,5 +18,5 @@ public interface WordDao {
     void deleteAll();
 
     @Query("SELECT * FROM word_table ORDER BY word ASC")
-    List<Word> getAllWords();
+    LiveData<List<Word>> getAllWords();
 }
