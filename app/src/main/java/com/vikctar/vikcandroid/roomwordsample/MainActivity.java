cecd83455aebd8dc8704e8f331cdbc1c2d794bd5
Vikctar <vikc.steve@gmail.com>
2019-08-18 01:06:10 +0300

    connect with the data

--- app/src/main/java/com/vikctar/vikcandroid/roomwordsample/MainActivity.java
+++ app/src/main/java/com/vikctar/vikcandroid/roomwordsample/MainActivity.java
@@ -1,20 +1,34 @@
 package com.vikctar.vikcandroid.roomwordsample;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    // a ViewModel
+    private WordViewModel mWordViewModel;
+
+    // request code
+    public static final int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,20 +37,52 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // get a ViewModel from the ViewModelProvider
+        mWordViewModel = ViewModelProviders.of(this).get(WordViewModel.class);
+
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
         final WordListAdapter adapter = new WordListAdapter(this);
 
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        // add an observer for the LiveData returned by getAllWords()
+        mWordViewModel.getAllWords().observe(this, new Observer<List<Word>>() {
+            @Override
+            public void onChanged(List<Word> words) {
+                // Update the cached copy of the words in the adapter
+                adapter.setmWords(words);
+            }
+        });
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this,
+                        NewWordActivity.class);
+                startActivityForResult(intent, NEW_WORD_ACTIVITY_REQUEST_CODE);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // If the activity returns with RESULT_OK, insert the returned word into the database.
+        if (requestCode == NEW_WORD_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            Word word = new Word(
+                    Objects.requireNonNull(
+                            data != null ? data.getStringExtra(NewWordActivity.EXTRA_REPLY) : null
+                    )
+            );
+            mWordViewModel.insert(word);
+        } else {
+            Toast.makeText(getApplicationContext(),
+                    R.string.empty_not_saved,
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
